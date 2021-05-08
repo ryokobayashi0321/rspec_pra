626d35bdb0dba8268b7359875fda8f848ce1d5d3
ryo <if14germany1248@icloud.com>
2021-05-08 20:07:52 +0900

    createにリクエストスペック実施

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user
+    else
+      render.new
+    end
   end
 
   def show
@@ -21,5 +27,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age, :email)
   end
 end
