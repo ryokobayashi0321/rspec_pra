eeeb3e8ef223c7fa14f9ab9fbabfb08e21e19ea9
ryo <if14germany1248@icloud.com>
2021-05-08 19:21:58 +0900

    showにリクエストスペック実施

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,39 @@ RSpec.describe "Users", type: :request do
       end
     end
   end
-end
+
+  describe "GET#show" do
+    subject {get(users_path(user_id))}
+    context "ユーザーが存在するとき" do
+      let(:user) {create(:user)}
+      let(:user_id) {user.id}
+
+      it "リクエストが成功する" do
+        subject
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "nameが表示されている" do
+        subject
+        expect(response.body).to include user.name
+      end
+
+      it "ageが表示されている" do
+        subject
+        expect(response.body).to include user.age.to_s
+      end
+
+      it "emailが表示されている" do
+        subject
+        expect(response.body).to include user.email
+      end
+    end
+
+    context ":idに対応するユーザーが存在しないとき"
+      let(:user_id){1}
+
+      it "エラーが発生する" do
+        expect{subject}.to raise_error ActiveRecord::RecordNotFound
+      end
+    end
+  end
